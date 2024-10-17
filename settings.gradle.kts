00d2c22b12979e3ddaf9be2da1aca778549215f8
Martin Bonnin <martin@mbonnin.net>
2024-10-17 13:06:58 +0200

    We need to build with Java <= 17 for now

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,10 @@ include(":gr8-plugin")
 include(":gr8-plugin-external")
 include(":gr8-plugin-common")
 
+/**
+ * We need Java <= 17 until we update the embedded gr8
+ * See https://issuetracker.google.com/u/2/issues/365578411
+ */
+check(JavaVersion.current() <= JavaVersion.VERSION_17) {
+  "This project needs to be run with Java 17 or higher (found: ${JavaVersion.current()})."
+}
